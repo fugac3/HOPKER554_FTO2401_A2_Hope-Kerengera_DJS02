1c6eb90e7dc48b955a74fdfd95ee2a2e20114266
Hope <k.hope.ing@gmail.com>
2024-05-09 19:04:02 +0200

    Rounding Result and Validation for Blank Inputs

--- HOPKER554_FTO2401_A2_Hope-Kerengera_DJS02/scripts.js
+++ HOPKER554_FTO2401_A2_Hope-Kerengera_DJS02/scripts.js
@@ -1,9 +1,26 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+/**
+ * @throws {error} - if any of the inputs are left blank or if any of the inputs are invalid
+ *
+ */
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+  //addEventListener method always takes two arguments, the type of event and the function that handles the event
+  event.preventDefault(); //necessary to prevent page from reloading on submit
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  if (!dividend || !divider) {
+    //Preventing empty inputs
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again.";
+    throw console.error(
+      "Division not performed. Both values are required in inputs. Try again."
+    );
+  } else if (typeof dividend !== "number") {
+  } else {
+    const calculation = dividend / divider;
+    result.innerText = Math.floor(calculation); //Math.floor to round DOWN to nearest integer - 1
+  }
 });
