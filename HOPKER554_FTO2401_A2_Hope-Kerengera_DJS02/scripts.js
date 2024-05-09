9778312ea47394cdabd2de6b1adc8e3ce0cfc03e
Hope <k.hope.ing@gmail.com>
2024-05-09 21:09:22 +0200

    Added Error Handling and Additional Input Validation (Only Numbers)

--- HOPKER554_FTO2401_A2_Hope-Kerengera_DJS02/scripts.js
+++ HOPKER554_FTO2401_A2_Hope-Kerengera_DJS02/scripts.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 /**
+ * @throws {error} - if divider is equal to 0
  * @throws {error} - if any of the inputs are left blank or if any of the inputs are invalid
  *
  */
@@ -18,8 +19,26 @@ form.addEventListener("submit", (event) => {
     throw console.error(
       "Division not performed. Both values are required in inputs. Try again."
     );
-  } else if (typeof dividend !== "number") {
-  } else {
+  } //if divider = 0
+  else if (divider == 0) {
+    result.innerText =
+      "Division not performed. Invalid number provided. Try again";
+    throw console.error(
+      "Division not performed. Invalid number provided. Try again"
+    );
+  }
+  //if input is not a number
+  else if (isNaN(dividend) || isNaN(divider)) {
+    //if dividend is not a number...
+    document.querySelector(
+      "body" //replace the innerHTML of the body with an error using <h1> tag
+    ).innerHTML = `<h1 style="color:red">Something critical went wrong. Please reload the page.<h1>`;
+    throw console.error(
+      "Something critical went wrong. Please reload the page."
+    );
+  }
+  //if result is a decimal, it should be rounded down
+  else {
     const calculation = dividend / divider;
     result.innerText = Math.floor(calculation); //Math.floor to round DOWN to nearest integer - 1
   }
